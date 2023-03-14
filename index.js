88400bdd8e5d0cee747e9fe2ce0ec4ffba92ec3d
Susana Muñoz <su.munozh@gmail.com>
2023-03-14 11:35:02 -0300

    Each

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ app.get("/",(req, res)=>{
 app.get("/contacto",(req, res)=>{
     res.render("contacto")
 })
+
+app.get("/equipos",(req, res)=>{
+    res.render("equipos", {equipos:[{nombre:"Barcelona"},
+                                    {nombre:"Inter"},
+                                    {nombre:"Liverpool"},
+                                    {nombre:"LaJuve"},
+                                    {nombre:"Magallanes"},
+                                    {nombre:"PSG"}]})
+})
+
+app.get("/f1",(req, res)=>{
+    res.render("f1", {escuderias:[{nombre:"Red Bull"},
+                                    {nombre:"Mercedez"},
+                                    {nombre:"Ferrari"},
+                                    {nombre:"Alpine"}]})
+})
